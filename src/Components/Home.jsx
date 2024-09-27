e5412fa749e1c5c857d541d18f9c5a64ec2852b4
manishsuthar94 <manishsutharr94@gmail.com>
2024-09-27 18:19:13 +0530

    text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -78,35 +78,47 @@ function Home() {
         </div>
       </div>
 
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="mx-auto max-w-3xl text-center">
-          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Trusted by Homeowners and Designers
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+            Trusted by the world’s most innovative teams
           </h2>
-
-          <p className="mt-4 text-gray-500 sm:text-xl">
-            Empire Design is the trusted choice for transforming homes and
-            workspaces with quality and style.
-          </p>
-        </div>
-
-        <div className="bg-white sm:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3 ">
-              {stats.map((stat) => (
-                <div
-                  key={stat.id}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4 rounded-lg border border-gray-200 p-7"
-                >
-                  <dt className="text-xl leading-7 text-gray-600">
-                    {stat.name}
-                  </dt>
-                  <dd className="order-first text-2xl font-semibold tracking-tight text-gray-900 sm:text-2xl">
-                    {stat.value}
-                  </dd>
-                </div>
-              ))}
-            </dl>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+            <img
+              alt="Transistor"
+              src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+              width={158}
+              height={48}
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            />
+            <img
+              alt="Reform"
+              src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+              width={158}
+              height={48}
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            />
+            <img
+              alt="Tuple"
+              src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+              width={158}
+              height={48}
+              className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            />
+            <img
+              alt="SavvyCal"
+              src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+              width={158}
+              height={48}
+              className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            />
+            <img
+              alt="Statamic"
+              src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+              width={158}
+              height={48}
+              className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            />
           </div>
         </div>
       </div>
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -5,17 +5,17 @@ function Shop() {
   const products = [
     {
       id: 1,
-      name: "Earthen Bottle",
+      name: "Wipro Furniture Gusto High Back Chair",
       href: "#",
       price: "$48",
       imageSrc:
-        "https://m.media-amazon.com/images/I/71cPrlBcO-L._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/61OpDrUEC4L._AC_UL320_.jpg",
       imageAlt:
         "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
     },
     {
       id: 2,
-      name: "Nomad Tumbler",
+      name: "Solid Sheesham Wood 6 Seater Sofa Set",
       href: "#",
       price: "$35",
       imageSrc:
@@ -25,61 +25,61 @@ function Shop() {
     },
     {
       id: 3,
-      name: "Focus Paper Refill",
+      name: "Wood Bedroom Furniture ",
       href: "#",
       price: "$89",
       imageSrc:
-        "https://m.media-amazon.com/images/I/610GhgEG6pL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/61sYtf2HNdL._AC_UL320_.jpg",
       imageAlt:
         "Person using a pen to cross a task off a productivity paper card.",
     },
     {
       id: 4,
-      name: "Machined Mechanical Pencil",
+      name: "Compact Plastic Cabinet for Storage",
       href: "#",
       price: "$35",
       imageSrc:
-        "https://m.media-amazon.com/images/I/61LOY61YKCL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/81yswKKDH3L._AC_UL320_.jpg",
       imageAlt:
         "Hand holding black machined steel mechanical pencil with brass tip and top.",
     },
     {
       id: 5,
-      name: "Earthen Bottle",
+      name: "Wood Dining Table 4 Seater ",
       href: "#",
       price: "$48",
       imageSrc:
-        "https://m.media-amazon.com/images/I/71uxQPlAKhL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/71DvmF5+xRL._AC_UL320_.jpg",
       imageAlt:
         "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
     },
     {
       id: 6,
-      name: "Nomad Tumbler",
+      name: "Double Bed Cot Bed Furniture for Bedroom",
       href: "#",
       price: "$35",
       imageSrc:
-        "https://m.media-amazon.com/images/I/519BKVV1yRL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/71Pl+x8UnvL._AC_UL320_.jpg",
       imageAlt:
         "Olive drab green insulated bottle with flared screw lid and flat top.",
     },
     {
       id: 7,
-      name: "Focus Paper Refill",
+      name: "Set of 2 Outdoor Seating Chairs",
       href: "#",
       price: "$89",
       imageSrc:
-        "https://m.media-amazon.com/images/I/71-dNLM2PSL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/81CgXzoF5wL._AC_UL320_.jpg",
       imageAlt:
         "Person using a pen to cross a task off a productivity paper card.",
     },
     {
       id: 8,
-      name: "Machined Mechanical Pencil",
+      name: "Sheesham Wood 5 Seater Sofa",
       href: "#",
       price: "$35",
       imageSrc:
-        "https://m.media-amazon.com/images/I/81XPnJCcFzL._AC_UL320_.jpg",
+        "https://m.media-amazon.com/images/I/71A6gFjHhtL._AC_UL320_.jpg",
       imageAlt:
         "Hand holding black machined steel mechanical pencil with brass tip and top.",
     },

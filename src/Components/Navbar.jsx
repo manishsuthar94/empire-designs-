e4677ae1d2c0b98c888f6c716df41447358a3d34
manishsuthar94 <manishsutharr94@gmail.com>
2024-09-28 12:57:09 +0530

    ss

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -77,14 +77,14 @@ export default function Navbar() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link to={"/"} className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               alt=""
               src="https://www.zarla.com/images/zarla-furniture-fixture-1x1-2400x2400-20210701-4jhh8kj7q79pyhjbhwfp.png?crop=1:1,smart&width=250&dpr=2"
               className="h-10 w-auto"
             />
-          </a>
+          </Link>
         </div>
 
         <div className="flex lg:hidden">
@@ -212,14 +212,14 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to={"/"} className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 alt=""
                 src="https://www.zarla.com/images/zarla-furniture-fixture-1x1-2400x2400-20210701-4jhh8kj7q79pyhjbhwfp.png?crop=1:1,smart&width=250&dpr=2"
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
 
             <button
               type="button"

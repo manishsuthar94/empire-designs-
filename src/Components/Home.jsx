34a58766bbc35104b7dba10af8817504540501f2
manishsuthar94 <manishsutharr94@gmail.com>
2024-09-28 08:47:29 +0530

    mm

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -58,10 +58,10 @@ function Home() {
               ></path>
             </svg>
           </a>
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">
             Transform Your Home with Timeless Furniture
           </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          <p className="mb-8 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
             Discover stylish, modern furniture that elevates every corner of
             your home. Find the perfect piece to match your unique style today.
           </p>

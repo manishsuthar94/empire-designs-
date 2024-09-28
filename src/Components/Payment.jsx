cda67f973f2061c822cec7b14aa625e900008366
manishsuthar94 <manishsutharr94@gmail.com>
2024-09-28 12:53:03 +0530

    ss

--- src/Components/Payment.jsx
+++ src/Components/Payment.jsx
@@ -95,7 +95,7 @@ function Payment() {
                     <div className="relative">
                       <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
                         <svg
-                          className="h-4 w-4 text-gray-500 dark:text-gray-400"
+                          className="h-4 w-4 text-gray-500"
                           aria-hidden="true"
                           xmlns="http://www.w3.org/2000/svg"
                           width="24"
@@ -115,7 +115,7 @@ function Payment() {
                         datepicker-format="mm/yy"
                         id="card-expiration-input"
                         type="text"
-                        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-9 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-9 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
                         placeholder="12/23"
                         required
                       />
@@ -130,7 +130,7 @@ function Payment() {
                       <button
                         data-tooltip-target="cvv-desc"
                         data-tooltip-trigger="hover"
-                        className="text-gray-400 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+                        className="text-gray-400 hover:text-gray-900 "
                       >
                         <svg
                           className="h-4 w-4"
@@ -149,7 +149,7 @@ function Payment() {
                       <div
                         id="cvv-desc"
                         role="tooltip"
-                        className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                        className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 "
                       >
                         The last 3 digits on back of card
                         <div className="tooltip-arrow" data-popper-arrow></div>
@@ -168,14 +168,14 @@ function Payment() {
 
                 <button
                   type="submit"
-                  className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 "
                 >
                   Pay now
                 </button>
               </form>
 
               <div className="mt-6 grow sm:mt-8 lg:mt-0">
-                <div className="space-y-4 rounded-lg border border-gray-100 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800">
+                <div className="space-y-4 rounded-lg border border-gray-100 bg-gray-50 p-6 ">
                   <div className="space-y-2">
                     {/* Remove non-numeric characters before conversion */}
                     {/* Get price as a number by removing any non-numeric characters (like $) */}
@@ -188,7 +188,7 @@ function Payment() {
                         <>
                           {/* Original price */}
                           <dl className="flex items-center justify-between gap-4">
-                            <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                            <dt className="text-base font-normal text-gray-500 ">
                               Original price
                             </dt>
                             <dd className="text-base font-medium text-gray-900 ">
@@ -198,7 +198,7 @@ function Payment() {
 
                           {/* Calculate 5% savings */}
                           <dl className="flex items-center justify-between gap-4">
-                            <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                            <dt className="text-base font-normal text-gray-500 ">
                               Savings (5% off)
                             </dt>
                             <dd className="text-base font-medium text-green-500">
@@ -208,7 +208,7 @@ function Payment() {
 
                           {/* Store pickup fee fixed at $2 */}
                           <dl className="flex items-center justify-between gap-4">
-                            <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                            <dt className="text-base font-normal text-gray-500 ">
                               Store Pickup
                             </dt>
                             <dd className="text-base font-medium text-gray-900 ">
@@ -218,7 +218,7 @@ function Payment() {
 
                           {/* Calculate 3% tax */}
                           <dl className="flex items-center justify-between gap-4">
-                            <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+                            <dt className="text-base font-normal text-gray-500 ">
                               Tax (3%)
                             </dt>
                             <dd className="text-base font-medium text-gray-900 ">
@@ -227,7 +227,7 @@ function Payment() {
                           </dl>
 
                           {/* Total price calculation */}
-                          <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
+                          <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 ">
                             <dt className="text-base font-bold text-gray-900 ">
                               Total
                             </dt>
@@ -283,12 +283,12 @@ function Payment() {
               </div>
             </div>
 
-            <p className="mt-6 text-center text-gray-500 dark:text-gray-400 sm:mt-8 lg:text-left">
+            <p className="mt-6 text-center text-gray-500  sm:mt-8 lg:text-left">
               Payment processed by{" "}
               <a
                 href="#"
                 title=""
-                className="font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
+                className="font-medium text-primary-700 underline hover:no-underline "
               >
                 Paddle
               </a>{" "}
@@ -296,7 +296,7 @@ function Payment() {
               <a
                 href="#"
                 title=""
-                className="font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
+                className="font-medium text-primary-700 underline hover:no-underline "
               >
                 Flowbite LLC
               </a>
